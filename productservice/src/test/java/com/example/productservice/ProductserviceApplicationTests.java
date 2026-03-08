c0371d1e9674b573e8636a3b055d08ee0fcfe828
Shoib Ansari <shoib.ansari@sap.com>
2026-03-08 17:51:05 +0530

    commented main test and added MockitBean

--- productservice/src/test/java/com/example/productservice/ProductserviceApplicationTests.java
+++ productservice/src/test/java/com/example/productservice/ProductserviceApplicationTests.java
@@ -1,19 +1,24 @@
 package com.example.productservice;
 
+import com.example.productservice.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class ProductserviceApplicationTests {
 
+    @MockitoBean
+    private ProductRepository productRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
-    @Test
-    void main(){
-        ProductserviceApplication.main(new String[]{});
-    }
+//    @Test
+//    void main(){
+//        ProductserviceApplication.main(new String[]{});
+//    }
 }
