5386c403f2d7ca4dadc455daf06f4737c148546c
Shoib Ansari <shoib.ansari@sap.com>
2026-03-15 12:29:30 +0530

    Address entity and AddressRepository added

---
+++ userservice/src/main/java/com/example/userservice/entity/Address.java
@@ -0,0 +1,59 @@
+package com.example.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "address" )
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(name = "is_default", nullable = false)
+    private boolean isDefault = false;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    private void onSave(){
+        this.updatedAt= LocalDateTime.now();
+        this.createdAt= LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt= LocalDateTime.now();
+    }
+
+}
